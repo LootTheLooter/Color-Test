aa7fc3a673b541f16249f6851261db764e201915
Loot <78662497+LootTheLooter@users.noreply.github.com>
2021-12-15 19:39:34 -0700

    Code Update
    The code looked pretty spaghetti, so I did some things to make it look a bit better.

--- index.js
+++ index.js
@@ -3,32 +3,33 @@
 // variables
 const randomButton = document.getElementById("randomColorButton");
 const resetColorButton = document.getElementById("resetButton");
-const HEXImport = document.getElementById("importButton");
 
+const HEXImport = document.getElementById("importButton");
 const hexText = document.getElementById("HEXText");
-const rSpan = document.getElementById("redSpan");
-const gSpan = document.getElementById("greenSpan");
-const bSpan = document.getElementById("blueSpan");
-const aSpan = document.getElementById("alphaSpan")
 
 const rSlider = document.getElementById("rSlider");
 const gSlider = document.getElementById("gSlider");
 const bSlider = document.getElementById("bSlider");
-const aSlider = document.getElementById("aSlider")
-const HEXInput = document.getElementById("HEXInput");
+const aSlider = document.getElementById("aSlider");
+
+const Sliders = document.getElementsByClassName("slider");
+const Spans = document.getElementsByClassName("sliderSpan");
 
 const RGBDiv = document.getElementById("Editable");
 
 // functions
 function randomRGB() {
   let rngNumber = Math.floor(Math.random() * 255) + 1;
-  let rgbText = "rgb(" + rngNumber.toString()
-  for (let i = 0; i < 1; i++) {
+  let rgbText = "rgb(" + rngNumber.toString();
+  
+  for (let i = 0; i < 2; i++) {
     rngNumber = Math.floor(Math.random() * 255) + 1;
-    rgbText += "," + rngNumber.toString()
+    rgbText += "," + rngNumber.toString();
   }
-  rgbText += "," + rngNumber.toString() + ")"
-  return rgbText
+  
+  rgbText += ")";
+
+  return rgbText;
 }
 
 function ChangeColor(item, bgColor, bdColor) {
@@ -62,40 +63,50 @@ function HEXtoRGB(hex) {
   } : null;
 }
 
-function sliderUpdate(slider, spanElement, text) {
-  spanElement.innerHTML = text + slider.value
-  ChangeColor(RGBDiv, ["rgba(", rSlider.value, ",", gSlider.value, ",", bSlider.value, ",", aSlider.value, ")"].join(""))
-  hexText.innerHTML = "HEX: " + rgbToHEX(Number(rSlider.value), Number(gSlider.value), Number(bSlider.value))
+function sliderUpdate() {  
+  for (let SpanIndex in Spans) {
+      if (Spans[SpanIndex].id !== "undefined" && Spans[SpanIndex].id !== undefined) {
+        let CurrentSpan = Spans[SpanIndex];
+        let colonPosition = CurrentSpan.innerHTML.indexOf(":") + 1;
+
+        CurrentSpan.innerHTML = CurrentSpan.innerHTML.substring(0, colonPosition) + " " + Sliders[SpanIndex].value;
+    }
+  }
+
+  ChangeColor(RGBDiv, ["rgba(", rSlider.value, ",", gSlider.value, ",", bSlider.value, ",", aSlider.value, ")"].join(""));
+  hexText.innerHTML = "HEX: " + rgbToHEX(Number(rSlider.value), Number(gSlider.value), Number(bSlider.value));
 }
 
-// connecting events
-randomButton.onclick = function() {ChangeColor(randomButton, randomRGB(), randomRGB())}
+// event stuff
+randomButton.onclick = function() {ChangeColor(randomButton, randomRGB(), randomRGB())};
+
+rSlider.addEventListener("input", sliderUpdate); 
+gSlider.addEventListener("input", sliderUpdate); 
+bSlider.addEventListener("input", sliderUpdate);
+aSlider.addEventListener("input", sliderUpdate);
+
+sliderUpdate();
 
 HEXImport.onclick = function() {
-  RGBDiv.style.backgroundColor = "rgb(" + HEXtoRGB(HEXInput.value).r + "," + HEXtoRGB(HEXInput.value).g + "," + HEXtoRGB(HEXInput.value).b + ")"
-  rSlider.value = HEXtoRGB(HEXInput.value).r
-  gSlider.value = HEXtoRGB(HEXInput.value).g
-  bSlider.value = HEXtoRGB(HEXInput.value).b
+  const HEXInput = document.getElementById("HEXInput");
 
-  sliderUpdate(rSlider, rSpan, "Red: ")
-  sliderUpdate(gSlider, gSpan, "Green: ")
-  sliderUpdate(bSlider, bSpan, "Blue: ")
+  RGBDiv.style.backgroundColor = "rgb(" + HEXtoRGB(HEXInput.value).r + "," + HEXtoRGB(HEXInput.value).g + "," + HEXtoRGB(HEXInput.value).b + ")";
+
+  rSlider.value = HEXtoRGB(HEXInput.value).r;
+  gSlider.value = HEXtoRGB(HEXInput.value).g;
+  bSlider.value = HEXtoRGB(HEXInput.value).b;
+
+  sliderUpdate();
 }
-
-rSlider.oninput = function() {sliderUpdate(rSlider, rSpan, "Red: ")};
-gSlider.oninput = function() {sliderUpdate(gSlider, gSpan, "Green: ")};
-bSlider.oninput = function() {sliderUpdate(bSlider, bSpan, "Blue: ")};
-aSlider.oninput = function() {sliderUpdate(aSlider, aSpan, "Alpha: ")}
-
-sliderUpdate(rSlider, rSpan, "Red: "); sliderUpdate(gSlider, gSpan, "Green: "); sliderUpdate(bSlider, bSpan, "Blue: "); sliderUpdate(aSlider, aSpan, "Alpha: ")
 
 resetColorButton.onclick = function() {
-  rSlider.value = "128"; rSpan.innerHTML = "Red: 128";
-  gSlider.value = "128"; gSpan.innerHTML = "Green: 128";
-  bSlider.value = "128"; bSpan.innerHTML = "Blue: 128";
-  aSlider.value = "1"; aSpan.innerHTML = "Alpha: 1";
+  rSlider.value = "128"; Spans[0].innerHTML = "Red: 128";
+  gSlider.value = "128"; Spans[1].innerHTML = "Green: 128";
+  bSlider.value = "128"; Spans[2].innerHTML = "Blue: 128";
+  aSlider.value = "1"; Spans[3].innerHTML = "Alpha: 1";
 
-  hexText.innerHTML = "HEX: " + rgbToHEX(Number(rSlider.value), Number(gSlider.value), Number(bSlider.value))
+  hexText.innerHTML = "HEX: #808080"
   RGBDiv.style.backgroundColor = "rgb(128, 128, 128)";
+  
+  sliderUpdate();
 }
-
